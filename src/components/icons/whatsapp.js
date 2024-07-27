e3a6545579cf3c7e728a8f00280f59fbf069d43f
Brian Makhembu <31996990+makhembu@users.noreply.github.com>
2024-07-27 23:50:53 +0300

    Update whatsapp.js

--- src/components/icons/whatsapp.js
+++ src/components/icons/whatsapp.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const IconWhatsApp = () => (
+const IconWhatsapp = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     role="img"
@@ -16,4 +16,4 @@ const IconWhatsApp = () => (
   </svg>
 );
 
-export default IconWhatsApp;
+export default IconWhatsapp;
